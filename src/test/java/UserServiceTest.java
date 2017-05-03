0290140282652a9fffacc51d7cbac60d59714c0b
zunzhuowei <1808862723@qq.com>
2017-05-03 23:37:43 +0800

    fix junit test bug

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -2,7 +2,8 @@
  * Created by zun.wei on 2016/10/6.
  * To change this template use File|Default Setting
  * |Editor|File and Code Templates|Includes|File Header
- */
+ *//*
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,17 +24,25 @@ import javax.inject.Inject;
 import java.util.Date;
 import java.util.List;
 
+*/
 /**
  * 声明用的是Spring的测试类
- **/
+ **//*
+
 @RunWith(SpringJUnit4ClassRunner.class)
-/** 声明spring主配置文件位置，注意：以当前测试类的位置为基准,有多个配置文件以字符数组声明 **/
+*/
+/** 声明spring主配置文件位置，注意：以当前测试类的位置为基准,有多个配置文件以字符数组声明 **//*
+
 @ContextConfiguration(locations = {"classpath*:spring-jpa.xml"})
-/** 声明使用事务，不声明spring会使用默认事务管理 **/
+*/
+/** 声明使用事务，不声明spring会使用默认事务管理 **//*
+
 //@Transactional
 //@Rollback
 //@Commit
-/** 声明事务回滚，要不测试一个方法数据就没有了岂不很杯具，注意：插入数据时可注掉，不让事务回滚 **/
+*/
+/** 声明事务回滚，要不测试一个方法数据就没有了岂不很杯具，注意：插入数据时可注掉，不让事务回滚 **//*
+
 //@TransactionConfiguration(transactionManager="transactionManager",defaultRollback=true)
 public class UserServiceTest {
 
@@ -69,12 +78,14 @@ public class UserServiceTest {
 
     @Test    // 查询分页对象
     public void testFindAllUserByPage() {
-        /**
+        */
+/**
          *  创建一个分页对象   （注意：0代表的是第一页，5代表每页的大小,后两个参数不写即为默认排序）
          *  Direction：为一个枚举类，定义了DESC和ASC排序顺序
          *  id：结果集根据id来进行DESC降序排序
          *  想自己实现的话，最好继承他这个类，来定义一些个性的方法
-         */
+         *//*
+
         PageRequest request = new PageRequest(1, 4, Sort.Direction.DESC, "id");
 
         Page<User> users = userService.findAllUserByPage(request);
@@ -126,3 +137,4 @@ public class UserServiceTest {
         }
     }
 }
+*/
